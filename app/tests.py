d4c67587c0499e05beda7c3661ff1a0bd0750f04
njerunjeru <njesh74@gmail.com>
2019-06-14 10:08:58 +0300

    changing ports to jhela to prevent timeout

--- app/tests.py
+++ app/tests.py
@@ -14,3 +14,20 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+import os
+import logging
+logger = logging.getLogger('ftpuploader')
+hdlr = logging.FileHandler('ftplog.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
+def logger():
+    try:
+        x = int(uu)
+        logger.error('Converted string to  int : '+ str(x))
+    except Exception as e :
+        logger.error('Failed to covert string to int : '+ str(e))
+        pass
